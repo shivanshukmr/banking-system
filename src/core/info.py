35167b5c6b3aaf7c9b1ef568559e1c46f0f857ba
Shivanshu Kumar <shivanshukmr007@gmail.com>
2020-08-22 12:33:41 +0530

    transaction hist and menu example

--- src/core/info.py
+++ src/core/info.py
@@ -1,3 +1,3 @@
 # get connection object and cursor object from dbconnector.connector
-# display other users w/ bankaccout nos. or uniqueid
-# display transaction history
+# display other users w/ bankaccout nos.
+# display transaction history(current user)
