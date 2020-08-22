3b863d24b6d81248b12be79b712e21530dad5411
Shivanshu Kumar <shivanshukmr007@gmail.com>
2020-08-22 12:59:57 +0530

    structure for functions created

--- src/core/users.py
+++ src/core/users.py
@@ -1,3 +1,17 @@
 # get connection object and cursor object from dbconnector.connector
 # user creation
 # user authentication/signin
+
+def usercreation():
+    '''
+    Gets name, password(twice), and updates db.
+    user gets auto-generated accno.
+    '''
+    pass
+
+def userauthentication():
+    '''
+    Gets account no., password and check in db
+    returns user object
+    '''
+    pass
