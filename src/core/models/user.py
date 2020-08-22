e06d8552143c159df236e0cd60d9ea1bd19cd87c
Shivanshu Kumar <shivanshukmr007@gmail.com>
2020-08-22 19:30:31 +0530

    created user class

--- src/core/models/user.py
+++ src/core/models/user.py
@@ -1,4 +1,14 @@
-# user class
-
 class User:
-    pass
+    def __init__(self, accno, username, balance):
+        self.accno = accno
+        self.username = username
+        self.balance = balance
+
+    def getusername(self):
+        return self.username
+    
+    def getaccno(self):
+        return self.accno
+    
+    def getbalance(self):
+        return self.balance
