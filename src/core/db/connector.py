659820eed7b5e4150b9a531836297c169702cacb
Shivanshu Kumar <shivanshukmr007@gmail.com>
2020-08-27 18:20:31 +0530

    connected to database

--- src/core/db/connector.py
+++ src/core/db/connector.py
@@ -1,1 +1,14 @@
 # connects to database and gives cursor, connection objects
+
+import mysql.connector
+
+with open("pass.txt", "r") as f:
+    output = f.readlines()
+
+db = mysql.connector.connect(
+    host="localhost",
+    user="root",
+    passwd=output[0].strip("\n"),
+    port=output[1],
+    database="bank"
+)
