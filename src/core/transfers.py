342622cdb906abd757554909a9451288fdb6ded2
cleavon2603 <cleavonolivera2603@gmail.com>
2020-09-08 08:42:18 +0530

    transfers

--- src/core/transfers.py
+++ src/core/transfers.py
@@ -5,16 +5,28 @@
 def deposite():
     "Deposit money to current users acc."
     money = int(input("money to be deposited"))
-    from connector import *
-    get_DB()
-    cursor = get_Cursor()
 
     # in users table
-    query = "update users set balance += money"
+    # acc is the account no. of the current user
+    query = "update users set balance += %s where accno=%s" % (money, acc)
+    cursor.execute(query)
+    db.commit
+
+    # in transaction table
 
 
 def withdraw():
     "Withdraw money from current users acc."
+    "Deposit money to current users acc."
+    money = int(input("money to be withdrawn"))
+
+    # in users table
+    # acc is the account no. of the current user
+    query = "update users set balance -= %s where accno=%s" % (money, acc)
+    cursor.execute(query)
+    db.commit
+
+    # in transaction table
 
 
 def transfer():
