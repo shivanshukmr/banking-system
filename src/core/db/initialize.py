6f8c30892905b0a895e9e5999fffdc8a73f43e70
cleavon2603 <cleavonolivera2603@gmail.com>
2020-09-14 13:42:30 +0530

    innitialize2

--- src/core/db/initialize.py
+++ src/core/db/initialize.py
@@ -18,3 +18,26 @@ cursor = db.cursor()
 query = "create database if not exists bank"
 cursor.execute(query)
 db.commit()
+
+query = '''create table bank.users
+    (firstname varchar(100) NOT NULL,
+    lastname varchar(100) NOT NULL,
+    accno int(6) PRIMARY KEY AUTO_INCREMENT,
+    passwd varchar(32) NOT NULL,
+    date_created datetime NOt NULL,
+    balance int)'''
+
+cursor.execute(query)
+db.commit()
+
+query = '''alter table bank.users auto_increment=100000'''
+cursor.execute()
+db.commit()
+
+query = '''create table bank.transactionhistory
+(
+    user1accno int(6),
+    user2accno int(6),
+    amount int(),
+    datetime datetime
+)
