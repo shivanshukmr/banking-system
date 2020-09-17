ed103f8ae6b8e59bd2cc13c89443e2dd5e3eefbb
Shivanshu Kumar <60311696+shivanshukmr@users.noreply.github.com>
2020-09-17 18:57:21 +0530

    finalized transactionhistory and users table

--- src/core/db/initialize.py
+++ src/core/db/initialize.py
@@ -1,9 +1,7 @@
 import mysql.connector
 import os.path
 
-
-pass_path = os.path.join(os.path.abspath(
-    os.path.dirname(__file__)), "pass.txt")
+pass_path = os.path.join(os.path.abspath(os.path.dirname(__file__)), "pass.txt")
 
 with open(pass_path, "r") as f:
     output = f.readlines()
@@ -19,7 +17,7 @@ query = "create database if not exists bank"
 cursor.execute(query)
 db.commit()
 
-query = '''create table bank.users
+query = '''create table if not exists bank.users
     (firstname varchar(100) NOT NULL,
     lastname varchar(100) NOT NULL,
     accno int(6) PRIMARY KEY AUTO_INCREMENT,
@@ -31,13 +29,24 @@ cursor.execute(query)
 db.commit()
 
 query = '''alter table bank.users auto_increment=100000'''
-cursor.execute()
+cursor.execute(query)
 db.commit()
 
-query = '''create table bank.transactionhistory
+query = '''create table if not exists bank.transactionhistory
 (
     user1accno int(6),
     user2accno int(6),
-    amount int(),
-    datetime datetime
-)
+    amount int not null,
+    datetime datetime not null,
+    FOREIGN KEY (user1accno) REFERENCES bank.users(accno)
+        ON UPDATE CASCADE
+        ON DELETE CASCADE,
+    FOREIGN KEY (user2accno) REFERENCES bank.users(accno)
+        ON UPDATE CASCADE
+        ON DELETE CASCADE
+)'''
+
+cursor.execute(query)
+db.commit()
+
+db.close()
