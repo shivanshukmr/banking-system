fc497cb98b47bd8c31801d8c135cbc0bb0e82f89
cleavon2603 <cleavonolivera2603@gmail.com>
2020-09-18 22:35:02 +0530

    added personal account details function

--- src/core/info.py
+++ src/core/info.py
@@ -1,5 +1,6 @@
 # get connection object and cursor object from dbconnector.connector
 # display other users w/ bankaccout nos.
+# display your account info
 # display balance
 # display transaction history(current user)
 
@@ -10,7 +11,21 @@ def getusers():
     cursor = get_Cursor()
 
     # acc is the account no. of the current user
-    query = "select name, lastname, accno from users where accno <> %s" % (
+    query = "select accno, firstname, lastname from users where accno <> %s" % (
+        acc,)
+    cursor.execute(query)
+    for row in cursor.fetchall():
+        print(row)
+
+
+def details():
+    "shows firstname, lastname accno. and balance"
+    from db.connector import *
+    get_DB()
+    cursor = get_Cursor()
+
+    # acc is the account no. of the current user
+    query = "select accno, firstname, lastname, balance, date_created from users where accno = %s" % (
         acc,)
     cursor.execute(query)
     for row in cursor.fetchall():
