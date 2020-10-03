b4cd9884ae8fb7030c3f61e815fae7678b532077
Shivanshu Kumar <60311696+shivanshukmr@users.noreply.github.com>
2020-10-03 19:43:02 +0530

    moved help options to assets directory

---
+++ src/core/assets/help_opts.py
@@ -0,0 +1,19 @@
+help_signedin = """
+Commands:
+    help                    : Show this help message and exit
+    transfer                : Transfer money
+    deposit                 : Deposit money
+    withdraw                : Withdraw money
+    details                 : Show all bank details
+    balance                 : Show current balance
+    transactionhistory      : Show transaction history
+    showusers               : Show other accounts
+    signout                 : Signout
+"""
+
+help_notsignedin = """
+Commands:
+    help                    : Show this help message and exit
+    signin                  : Sign in to your bank account
+    createaccount           : Create a new bank account
+"""
--- src/main.py
+++ src/main.py
@@ -1,21 +1,37 @@
+from core.assets.help_opts import help_notsignedin, help_signedin
+from core.info import balance
 from core.models.user import User
-from core.models.menu_txt import *
+from core.users import userauthentication
 
 user = None
+firsttime = True
+
+
 while True:
     # menu here
-    print("Type 'help' to see the list of commands")
+    if firsttime:
+        # only print when its the first time
+        # get asciiart
+        with open("core/assets/asciiart.txt", "r") as f:
+            asciiart = f.read()
+
+        print(asciiart)
+        print("BankCLI v1.0")
+        print("Type 'help' to see the list of commands.\n")
+        firsttime = False
+
     if isinstance(user, User):
         # signed in
-        command = input()
+        command = input(">> ")
         if command == "help":
-            print(line1)
+            print(help_signedin)
+        elif command == "balance":
+            balance(user)
 
-        pass
     else:
         # not signed in
-        command = input()
+        command = input(">> ")
         if command == "help":
-            print(line2)
-        pass
-    break
+            print(help_notsignedin)
+        elif command == "signin":
+            user = userauthentication()
