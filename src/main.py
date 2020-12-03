8a3b256f1018ce0dbbf376c1839e43449623a6b8
cleavon2603 <cleavonolivera2603@gmail.com>
2020-12-03 18:14:02 +0530

    window layout

--- src/main.py
+++ src/main.py
@@ -1,5 +1,17 @@
-import os.path
 from core.utils.mysqllogin import check_connection, get_mysql_credentials
+from core.utils.info import balance, details, getusers, transactionHistory
+from core.models.user import User
+from core.utils.users import userauthentication, usercreation
+from core.utils.transfers import deposit, withdraw, transfer
+from core.assets.assets import bankcli_asciiart, help_notsignedin, help_signedin
+from core.db.initialize import initialize_db
+from tkinter import *
+import os.path
+
+#======main window=====
+root = Tk()
+root.title("Banking System")
+root.configure(bg="#0859c6")
 
 credentials_path = os.path.join(
     os.path.abspath(os.path.dirname(__file__)), "core/db/mysqlcredentials.txt"
@@ -15,15 +27,11 @@ else:
     if not check_connection(output[0], output[1], output[2], output[3]):
         get_mysql_credentials()
 
-from core.assets.assets import bankcli_asciiart, help_notsignedin, help_signedin
-from core.db.initialize import initialize_db
+
 
 initialize_db()
 
-from core.utils.info import balance, details, getusers, transactionHistory
-from core.models.user import User
-from core.utils.users import userauthentication, usercreation
-from core.utils.transfers import deposit, withdraw, transfer
+
 
 user = None
 
@@ -32,42 +40,41 @@ print("BankCLI v1.0")
 print("Type 'help' to see the list of commands.\n")
 
 while True:
-    command = input(">> ")
-    command = command.strip().lower()
-
     if isinstance(user, User):
         # signed in
-        if command == "help":
-            print(help_signedin)
-        elif command == "transactionhistory":
-            transactionHistory(user)
-        elif command == "balance":
-            balance(user)
-        elif command == "details":
-            details(user)
-        elif command == "showusers":
-            getusers(user)
-        elif command == "deposit":
-            deposit(user)
-        elif command == "withdraw":
-            withdraw(user)
-        elif command == "signout":
-            user = None
-            print("You have signed out.")
-        elif command == "transfer":
-            transfer(user)
-        elif command == "exit":
-            break
+        # if command == "help":
+        #     print(help_signedin)
+        # elif command == "transactionhistory":
+        #     transactionHistory(user)
+        # elif command == "balance":
+        #     balance(user)
+        # elif command == "details":
+        #     details(user)
+        # elif command == "showusers":
+        #     getusers(user)
+        # elif command == "deposit":
+        #     deposit(user)
+        # elif command == "withdraw":
+        #     withdraw(user)
+        # elif command == "signout":
+        #     user = None
+        #     print("You have signed out.")
+        # elif command == "transfer":
+        #     transfer(user)
+        # elif command == "exit":
+        #    break
+
 
     else:
-        # not signed in
-        if command == "help":
-            print(help_notsignedin)
-        elif command == "createaccount":
-            usercreation()
-        elif command == "signin":
-            user = userauthentication()
-        elif command == "reconfigure":
-            get_mysql_credentials()
-        elif command == "exit":
-            break
+        # # not signed in
+        # if command == "help":
+        #     print(help_notsignedin)
+        # elif command == "createaccount":
+        #     usercreation()
+        # elif command == "signin":
+        #     user = userauthentication()
+        # elif command == "reconfigure":
+        #     get_mysql_credentials()
+        # elif command == "exit":
+        #   break
+root.mainloop()
