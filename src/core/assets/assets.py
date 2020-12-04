344f6f1da39c5216ec777be5deeb30c3eaf58c1e
cleavon2603 <cleavonolivera2603@gmail.com>
2020-12-04 23:32:37 +0530

    assets changes

--- src/core/assets/assets.py
+++ src/core/assets/assets.py
@@ -7,5 +7,5 @@ font_c = "White"  # default font colour that would be used in the window
 box = "#badbff"  # default textbox coour that would be used in the window
 icon = "banking-system\src\core\images\icon.ico"  # icon of the window (15x15, 32x32 or 64x64), nedds to be in .ico form
 bankcli_asciiart = ImageTk.PhotoImage(
-    Image.open("banking-system\src\core\images\cliart.png")
+    Image.open("banking-system\src\core\images\logo.png")
 )  # bankname image
