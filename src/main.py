4158a2ae2a6570c03865e6efe320caec4e8d50fe
Shivanshu Kumar <60311696+shivanshukmr@users.noreply.github.com>
2020-12-05 10:06:57 +0530

    cleaned up main

--- src/main.py
+++ src/main.py
@@ -1,46 +1,22 @@
-from core.utils.mysqllogin import check_connection, get_mysql_credentials
-from core.utils.info import balance, details, getusers, transactionHistory
-from core.tables.user import User
-from core.utils.users import userauthentication, usercreation
-from core.utils.transfers import deposit, withdraw, transfer
-from core.assets.assets import colour, font_c, icon, bankcli_asciiart
-from core.db.initialize import initialize_db
-from tkinter import *
-import os.path
+from tkinter import Label, Tk
+from gui.assets.assets import colour, font_c, icon, bankcli_asciiart
 
 # ======main window=====
 root = Tk()
 root.title("Banking System")
 root.configure(bg=colour)
 root.iconbitmap(icon)
-title_image = Label(root,image=bankcli_asciiart)
+title_image = Label(root, image=bankcli_asciiart)
 title_image.pack()
 version = Label(root, text="version 2.0", bg=colour, fg=font_c)
 version.grid(row=2, column=1)
+    
+# ======mysql credentials=====
 
+# ======initialize db=====
 
-#======credentials check=====
-credentials_path = os.path.join(
-    os.path.abspath(os.path.dirname(__file__)), "core/db/mysqlcredentials.txt"
-)
-# if file doesn't exist
-if not os.path.exists(credentials_path):
-    get_mysql_credentials()
-else:
-    with open(credentials_path, "r") as f:
-        output = [word.strip("\n") for word in f.readlines()]
+from core.db.initialize import initialize_db
 
-    # if connection is unsuccessful
-    if not check_connection(output[0], output[1], output[2], output[3]):
-        get_mysql_credentials()
 initialize_db()
 
-
-#=====functions========
-user = None
-while True:
-    if isinstance(user, User):
-        # signed in
-    else:
-        # # not signed in
 root.mainloop()
