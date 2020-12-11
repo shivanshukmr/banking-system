176a6ada3c7f730c7d5bced5c3e25246a21e1ade
Shivanshu Kumar <60311696+shivanshukmr@users.noreply.github.com>
2020-12-11 19:28:58 +0530

    todo updated, info cleaned

--- src/core/utils/info.py
+++ src/core/utils/info.py
@@ -2,34 +2,26 @@ from core.db.connector import get_Cursor
 from core.tables.transaction import Transaction
 
 
-def get_users(user):
+def get_users(accno):
     """
     Return other users w/ bankaccount nos.
     """
     cursor = get_Cursor()
 
     # acc is the account no. of the current user
-    acc = user.accno
-    query = "select accno, firstname, lastname from users where accno <> %s" % (acc,)
+    query = "select accno, firstname, lastname from users where accno <> %s" % (accno,)
     cursor.execute(query)
     return cursor.fetchall()
 
 
-def get_balance(user):
+def get_transactionhistory(accno):
     """
-    Return balance of current user
-    """
-    return user.balance
-
-
-def get_transactionhistory(user):
-    """
-    Return transaction history of current user
+    Return transaction history of given acc no.
     """
     cursor = get_Cursor()
     cursor.execute(
         "SELECT * FROM transactionhistory WHERE user1accno = {0} OR user2accno = {0} ORDER BY time_of_transaction DESC".format(
-            user.accno,
+            accno,
         )
     )
     transaction_hist = []
